7571503fd862fa47f30478d77b0a71a4808ed3ff
Ekaterina <kisaiksa@yandex.ru>
2024-08-29 08:17:21 +0300

    Добавление интерфейса для DirectorDbStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DirectorStorage extends Storage<Director> {
+
+    Optional<Director> findById(final Long id);
+    Collection<Director> findAll();
+}
